4a521d948c94fcc6d8cb106f95ba33a0cc5464b3
Dean <deaneffi1414@gmail.com>
2022-10-02 23:24:40 +0300

    add design, draw option and disable on result

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Tic-Tac-Toe
+
+live - https://shimshon1.github.io/Tic-Tac-Toe/
--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 let createPlayer = (sign, cellsChosen, turn = false) => { return {sign,cellsChosen,turn,} }
-
 let player1 = createPlayer("X", [], true)
 let player2 = createPlayer("O", [] )
-
 let gameboard = ['','','','','','','','','']
 let allCells = document.querySelectorAll(".game-cell")
+let gameboardDisplay = document.querySelector(".game-board")
 allCells = Array.from(allCells)
 
+let plays = -1;
 let wonText = document.querySelector(".won")
 
 
@@ -15,7 +15,6 @@ winOptions = [
     [1,4,7],[2,5,8],[0,4,8],[2,4,6]
 
 ]
-
 
 function checkWinner(){
 
@@ -35,6 +34,10 @@ function checkWinner(){
             if(correct >= 3){
                 console.log("player " + playersList[i].sign + ' won!')
                 wonText.textContent = "player " + playersList[i].sign + ' won!'
+                plays = 0
+                gameboardDisplay.style.pointerEvents = 'none'
+                gameboardDisplay.style.backgroundImage = 'linear-gradient(220deg, rgba(121, 121, 121,0.5),rgba(196, 186, 186,0.5))';
+
                 return true
             }
           
@@ -42,13 +45,19 @@ function checkWinner(){
 
     }
 
+    plays++
 
+    if (plays > 9){
+        wonText.textContent = "Its a draw!"
+        gameboardDisplay.style.pointerEvents = 'none'
 
+        gameboardDisplay.style.BackgroundImage = 'linear-gradient(220deg, green,grey)'
+        
+    }
 }
 
-
-
 function updateBoard(){
+
 
     for (cell in allCells){
         allCells[cell].textContent = gameboard[cell]
@@ -80,13 +89,26 @@ for (let i = 0; i < allCells.length; i++) {
             }
 
         }
-       
-
-
     })
    
 
     
 }
-updateBoard()
 
+function resetGame(){
+    player1 = createPlayer("X", [], true)
+    player2 = createPlayer("O", [] )
+    gameboard = ['','','','','','','','','']
+    wonText.textContent = 'Waiting for a result...'
+    gameboardDisplay.style.pointerEvents = 'all'
+    plays = 0
+    gameboardDisplay.style.backgroundImage = "linear-gradient(220deg, rgb(40, 40, 122),grey)"
+    updateBoard()
+    
+}
+
+
+let resetBtn = document.querySelector("button")
+
+resetBtn.addEventListener("click", resetGame)
+updateBoard()
